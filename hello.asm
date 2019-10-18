640505b1a6ece0014e0ede51333f13c397dfe946
Erwan Fraisse <efraisse@cs.ucsb.edu>
2019-10-18 13:10:04 -0700

    weird bugs

--- hello.asm
+++ hello.asm
@@ -3,13 +3,37 @@
 #
 #  Data Area - allocate and initialize variables
 .data
-	# TODO: Write your string definitions here
+    string1: .ascii "Choose an integer number between 0 and 1000:\n"
+    string2: .ascii "Hello World!\n"
+    string3: .ascii "User chose"
+    string4: .ascii ". Truly a wise choice.\n"
 
 #Text Area (i.e. instructions)
 .text
 main:
+    li $v0, 4
+    la $a0, string1
+    syscall
 
-	# TODO: Write your code here
+    li $v0, 5
+    move $t0, $v0
+    syscall
+
+    li $v0, 4
+    la $a0, string2
+    syscall
+
+    li $v0, 4
+    la $a0, string3
+    syscall
+
+    li $v0, 1
+    syscall
+    move $a0, $t0
+    
+    li $v0, 4
+    la $a0, string4
+    syscall
 
 exit:
 	# Exit
